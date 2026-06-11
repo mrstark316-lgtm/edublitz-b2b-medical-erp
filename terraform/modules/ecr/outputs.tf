3b01a12478b787e570ef840626c484092f7180a3
mrstark316-lgtm <mrstark316@gmail.com>
2026-06-11 16:25:41 +0530

    changes-push

--- terraform/modules/ecr/outputs.tf
+++ terraform/modules/ecr/outputs.tf
@@ -1,6 +1,10 @@
 output "repository_urls" {
-  value = { for k, v in aws_ecr_repository.services : k => v.repository_url }
+  value = {
+    for k, v in aws_ecr_repository.services :
+    k => v.repository_url
+  }
 }
+
 output "registry_id" {
   value = values(aws_ecr_repository.services)[0].registry_id
 }
